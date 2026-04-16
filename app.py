7c6b2a62a7c5feb9ab2aa86af29e999f090f0e1c
Erick Carlson <erick.m.carlson@ndsu.edu>
2026-04-16 11:44:56 -0500

    Update app.py

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ st.title("FIN 330 - Final Project")
 
 st.markdown("""
 Created by: Erick Carlson, Philip Job, and Wyatt Paggen
+
 Welcome! **Use the sidebar to navigate between pages**
 
 ### Pages
